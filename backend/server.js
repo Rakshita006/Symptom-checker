93904a2d35b29b1487c5206a228ea1d9a5181405
Rakshita Kumari <rakshita0903@gmail.com>
2026-05-04 09:16:30 +0530

    updated for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ dotenv.config()
 
 const app=express()
 
-app.use(cors())
+app.use(cors({
+  origin:[ 'https://symptom-checker-lime-psi.vercel.app',
+    'http://localhost:5173'
+  ]
+}))
 
 app.use(express.json())
 
